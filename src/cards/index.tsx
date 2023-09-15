3ee0cbc3c2a9b3932e20ab9878338d6fa5afefa8
unknown <camosso26@gmail.com>
2023-09-15 11:30:13 +0100

    index alterado

--- src/cards/index.tsx
+++ src/cards/index.tsx
@@ -1,8 +1,14 @@
 import NormalCard from './NormalCard'
 import {CardFiltro} from './CardFiltro'
 import {CheckListCard} from '@/cards/CardFiltro/CheckListCard'
+import {SuperCard} from '@/cards/SuperCard'
+import {CommersCard} from '@/cards/CommersCard'
+import {RecomendedCard} from '@/cards/RecomendedCard'
 export const Card={
    NormalCard: NormalCard,
    CardFiltro:CardFiltro,
-   CheckListCard:CheckListCard
+   SuperCard:SuperCard,
+   CheckListCard:CheckListCard,
+   CommersCard:CommersCard,
+   RecomendedCard:RecomendedCard
 }
