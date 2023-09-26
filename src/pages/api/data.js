54738e15ca8d4d95e8096440b1080c1a58fb9ec0
unknown <camosso26@gmail.com>
2023-09-26 16:17:15 +0100

    adicao de um array com novos produtos

---
+++ src/pages/api/data.js
@@ -0,0 +1,207 @@
+export const categories = [
+    {
+      category: 'pc-gamer',
+      url: './assets/img/1(13).png'
+    },
+    {
+      category: 'Home Office',
+      url: './assets/img/1(1).png'
+    },
+    {
+      category: 'Workstation',
+      url: './assets/img/1(2).png'
+    },
+    {
+      category: 'Periféricos',
+      url: './assets/img/1(3).png'
+    },
+    {
+      category: 'Hardware',
+      url: './assets/img/1(4).png'
+    },
+    {
+      category: 'Setup completo',
+      url: './assets/img/1(5).png'
+    },
+    {
+      category: 'Cadeira',
+      url: './assets/img/1(6).png'
+    },
+    {
+      category: 'acessorio',
+      url: './assets/Assets_product/cooler.webp'
+    },
+    {
+      category: 'Pc para Stream',
+      url: './assets/img/1(25).png'
+    }
+  ]
+
+export const imageUrls =  [
+	{
+		url: '../assets/img/1(10).png',
+	},
+	{
+		url: '../assets/img/1(12).png'
+	},
+	{
+		url: '../assets/img/1(5).png'
+	},
+	{
+		url: '../assets/img/1(2).png'
+	}
+]
+
+export const marca=
+["UNYKA","NOX","NTECH","Neologic"]
+
+export const product=[
+  {
+    id:1,
+    tipo:"acessorio",
+    name:"WATER COOLER UNYKA GAMING AQUASTORM 240MM",
+    cod_post:"082731",
+    referencia:"UK352101",
+    marca:marca[0],
+    price:"66,90",
+    desconto:"10%",
+    quantidade:12,
+    img:"../assets/Assets_product/cooler.webp"
+  },
+  {
+    id:2,
+    tipo:"acessorio",
+    name:"COOLER CPU NOX HUMMER H-120 ARGB WATERCOOLER 120MM",
+    cod_post:"082731",
+    referencia:"NXHUMMERH120ARGB",
+    marca:marca[1],
+    price:"66,90",
+    desconto:"11%",
+    quantidade:12,
+    img:"../assets/Assets_product/4597_2___page_800_800.webp"
+  },
+  {
+    id:3,
+    tipo:"acessorio",
+    name:"WATER COOLER CPU 360 ARGB + AUTO RGB LGA 1700 READY 394X120X52MM AMD/INTEL/UNIVERSAL SLAYER RX360 ARGB",
+    cod_post:"082731",
+    referencia:"RX360A40",
+    marca:marca[2],
+    price:"100,90",
+    desconto:"10%",
+    quantidade:12,
+    img:"../assets/Assets_product/rx360a40___page_800_800.webp"
+  }, {
+    id:6,
+    tipo:"acessorio",
+    name:"WATER COOLER UNYKA GAMING AQUASTORM 240MM",
+    cod_post:"082731",
+    referencia:"UK352101",
+    marca:marca[2],
+    price:"13%",
+    desconto:"306,80",
+    quantidade:12,
+    img:"../assets/Assets_product/cooler.webp"
+  },
+  {
+    id:4,
+    tipo:"acessorio",
+    name:"COOLER CPU NOX HUMMER H-120 ARGB WATERCOOLER 120MM",
+    cod_post:"082731",
+    referencia:"NXHUMMERH120ARGB",
+    marca:marca[0],
+    price:"66,90",
+    desconto:"15%",
+    quantidade:12,
+    img:"../assets/Assets_product/4597_2___page_800_800.webp"
+  },
+  {
+    id:5,
+    tipo:"acessorio",
+    name:"WATER COOLER CPU 360 ARGB + AUTO RGB LGA 1700 READY 394X120X52MM AMD/INTEL/UNIVERSAL SLAYER RX360 ARGB",
+    cod_post:"082731",
+    referencia:"RX360A40",
+    marca:marca[0],
+    price:"100,90",
+    desconto:"1%",
+    quantidade:12,
+    img:"../assets/Assets_product/rx360a40___page_800_800.webp"
+  },
+  {
+    id:6,
+    tipo:"pc-gamer",
+    name:"Pc Gamer AMD Ryzen 5 5600G 16GB (Radeon Vega 7 Integrado) SSD 240GB, 500W 80 Plus, Neologic",
+    cod_post:"082731",
+    referencia:"RX360A40",
+    marca:marca[3],
+    price:"341060,90",
+    desconto:"80%",
+    quantidade:12,
+    img:"../assets/img/1 (3).png"
+  },
+  {
+    id:7,
+    tipo:"pc-gamer",
+    name:"Pc Gamer AMD Ryzen 5 5600G 16GB Radeon Vega 7 Integrado) SSD 240GB, 500W 80 Plus, Neologic",
+    cod_post:"082731",
+    referencia:"RX360A40",
+    marca:marca[3],
+    price:"661060,90",
+    desconto:"80%",
+    quantidade:12,
+    img:"../assets/img/1 (5).png"
+  },
+  {
+    id:8,
+    tipo:"pc-gamer",
+    name:"Pc Gamer AMD Ryzen 5 5600G 16GB (Radeon Vega 7 Integrado) Neologic",
+    cod_post:"082731",
+    referencia:"RX360A40",
+    marca:marca[3],
+    price:"1060,90",
+    desconto:"80%",
+    quantidade:12,
+    img:"../assets/img/1(1).png"
+  },
+  {
+    id:9,
+    tipo:"pc-gamer",
+    name:"Pc Gamer AMD (Radeon Vega 7 Integrado) SSD 240GB, 500W 80 Plus, Neologic",
+    cod_post:"082731",
+    referencia:"RX360A40",
+    marca:marca[3],
+    price:"90060,90",
+    desconto:"80%",
+    quantidade:12,
+    img:"../assets/img/1(9).png"
+  },
+  {
+    id:10,
+    tipo:"pc-gamer",
+    name:"Pc Gamer AMD Ryzen 5 5600G 16GB (R) SSD 240GB, Neologic",
+    cod_post:"082731",
+    referencia:"RX360A40",
+    marca:marca[3],
+    price:"23060,90",
+    desconto:"80%",
+    quantidade:12,
+    img:"../assets/img/1(11).png"
+  },
+  {
+    id:11,
+    tipo:"pc-gamer",
+    name:"Pc Gamer SSD 240GB, 500W 80 Plus, Neologic",
+    cod_post:"082731",
+    referencia:"RX360A40",
+    marca:marca[3],
+    price:"10060,90",
+    desconto:"80%",
+    quantidade:12,
+    img:"../assets/img/1(12).png"
+  },
+]
+
+
+export const inform=[
+  1,2,3,4,5,6,7,8,9,10
+]
