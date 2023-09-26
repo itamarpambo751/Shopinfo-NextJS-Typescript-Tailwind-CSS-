5991900cf5c25cd17c579c60b9c914650269c2d8
unknown <camosso26@gmail.com>
2023-09-26 16:14:43 +0100

    rota que me traz produtos apartir das categorias

---
+++ src/pages/search/[type]/index.tsx
@@ -0,0 +1,117 @@
+import React, { useState,useEffect } from 'react';
+import { useRouter } from 'next/router';
+import { Shopinfo } from "@/components";
+import { categories } from '@/pages/api/data';
+import { Banner, Hr,Categories, IconButton, PublicityCard, PublicityLabel, Section, Header } from '../../../components/_ui';
+import { CaretLeft, CaretRight, Heart, Truck } from 'phosphor-react';
+import { BsShare, BsCart4 } from 'react-icons/bs';
+import {VscThumbsup,VscThumbsdown} from 'react-icons/vsc'
+import { ComputerImageList, ComputerDescription } from '../../../components/pc-gamer';
+
+import { Card } from '@/cards';
+import { product } from '@/pages/api/data';
+import { marca } from '@/pages/api/data';
+
+
+export default function SearchProductAll(){
+	const router = useRouter();
+	const { type } = router.query;
+	const typeItems = typeof type === 'string' ? type : '';
+
+
+    const [Types, setTypes] = useState<any>([])
+    const [products, setProducts] = useState<product>(()=>{
+      return product
+   })
+
+
+useEffect(() => {
+
+   setTypes(products.filter((i)=>i.tipo===typeItems))
+   
+  },[products]); 
+
+  console.log(Types)
+
+  return (
+<>
+    <Header />
+{
+  Types[0]===undefined?
+  <div className='w-full p-10 flex items-center justify-center text-[30px] font-bold'>
+   
+    <p>Ops!...Items Nao Encontrados!</p>
+
+  </div>:
+  <div className='flex items center  justify-between w-[100%]'>
+  <div className='flex font-bold text-[13px] items-end'>
+    <p>Filtro</p>
+  </div>
+   <div className='w-[100%] flex justify-end items-center mt-[60px]'>
+           <div className='w-[250px] bg-[var(--shadow-color)] p-2 h-[80px] border-[1px] border-[var(--shadow-color)] flex flex-col justify-start items-center'>
+             <p className='uppercase text-[14px] font-semibold w-[100%]'>ordenar por:</p>
+               <select className='w-[100%] text-[#3e3e3e] border-none bg-transparent'>
+                 <option>selecione</option>
+               </select>
+           </div>
+      </div>
+</div>
+}
+
+
+    <main className='flex justify-between w-[100%] mt-[10px]'>
+
+
+<div className='w-[18%]'>
+{
+  Types[0]===undefined?
+  <div className='w-full flex items-center justify-center text-[30px] font-bold'>
+   
+
+
+  </div>:
+  <Card.CardFiltro label='marcas'
+    
+    options={
+      marca.map((i)=>
+      (<Card.CheckListCard key={i.id} item={i.toString()} />))
+    }
+    
+    />
+ }
+ 
+ 
+
+</div>
+
+
+<div className='w-[80%] flex flex-col'>
+<div className='w-[100%] p-2 grid grid-cols-4 gap-4 max-md:flex max-md:flex-wrap max-md:gap-[4px]'>
+{
+  Types[0]===undefined?
+  <div className='w-full flex items-center justify-center text-[30px] font-bold'>
+   
+
+  </div>:
+    Types.map((i)=>
+    (<Card.NormalCard key={i.id} imglLink={i.img} id={i.id.toString()} name={i.name} price={i.price} desconto={i.desconto}  />))
+}
+      
+</div>
+{
+  Types[0]===undefined?
+  <div className='w-full flex items-center justify-center text-[30px] font-bold'>
+
+  </div>:<div className='w-[100%] flex items-center justify-center mt-[20px] h-[300px]'>
+       <PublicityLabel.Root expires={true}>
+               <PublicityLabel.Content extrabold={true} bigText="Carregar mais produtos" bold/>
+      </PublicityLabel.Root>
+</div>
+}
+
+</div>
+
+</main>
+</>
+  )
+}
